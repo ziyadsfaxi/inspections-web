22a63c5de79370502543f195d11dbc4217fa04ba
ziyad <ziyadsfaxi@gmail.com>
2020-05-03 13:09:28 +0800

    add bootstrap to main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount("#app");
